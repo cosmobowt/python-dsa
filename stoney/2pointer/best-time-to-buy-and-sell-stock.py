5e6f0492f4188e7b7ec63da4ba9b6ecc6686879b
cosmobowt <anishsp21@gmail.com>
2026-07-12 02:57:28 +0530

    best time to buy and sell stock

---
+++ stoney/2pointer/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l = 0
+        r = 1
+        mx = 0
+
+        while(r<len(prices)):
+            if(prices[r]>prices[l]):
+                mx = max(mx,prices[r]-prices[l]);
+            else:
+                l=r
+            r+=1
+        return mx
+    
+sol = Solution();
+print(sol.maxProfit([7,1,5,3,6,4]));
